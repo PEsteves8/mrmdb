22cf262f30d9a760c131bf4c403d7bab349d1eb1
paulo.esteves <paulo.esteves@markdata.pt>
2017-06-09 15:22:52 +0100

    Added api key; updated dependencies

--- app/js/services/moviesService.js
+++ app/js/services/moviesService.js
@@ -3,10 +3,10 @@ export default class moviesService {
         this.$http = $http;
     }
     searchMovies(searchValue, page) {
-        return this.$http.get('http://www.omdbapi.com/?s=' + searchValue + '&page=' + page);
+        return this.$http.get('http://www.omdbapi.com/?s=' + searchValue + '&page=' + page + "&apikey=1fd9be66");
     }
     searchMovieInfo(movieId) {
-      return this.$http.get('http://www.omdbapi.com/?tomatoes=true&i=' + movieId);
+      return this.$http.get('http://www.omdbapi.com/?tomatoes=true&i=' + movieId + "&apikey=1fd9be66");
     }
 }
 
